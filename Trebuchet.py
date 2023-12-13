93ba35ae324f401ef2dd170a5b06839bb8791abd
Mohgwyn <fjgarciacobo@gmail.com>
2023-12-13 16:35:20 +0100

    Calcular la suma de los códigos de cada línea

--- Trebuchet.py
+++ Trebuchet.py
@@ -3,13 +3,15 @@ import re
 
 def main(input):
     digit_pattern = re.compile(r"\d")
+    sum = 0
+
     for line in input.splitlines():
         matches = digit_pattern.findall(line)
         print(f"El codigo es {matches[0]}{matches[-1]}")
+        sum += int(matches[0] + matches[-1])
 
 
 if __name__ == "__main__":
-    input = """1ab32c3
-    uah2ihcu7gau
-    178h67huvya23wdw"""
+    with open("input_1.txt") as f:
+        input = f.readlines
     main(input)

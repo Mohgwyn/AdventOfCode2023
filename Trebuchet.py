debcd7809d547aee05cea60369cef9a2c378ccec
Mohgwyn <fjgarciacobo@gmail.com>
2023-12-13 16:38:58 +0100

    ahora saca la suma con un print y se ha iterado sobre el conjunto de lineas

--- Trebuchet.py
+++ Trebuchet.py
@@ -5,13 +5,15 @@ def main(input):
     digit_pattern = re.compile(r"\d")
     sum = 0
 
-    for line in input.splitlines():
+    for line in input:
         matches = digit_pattern.findall(line)
-        print(f"El codigo es {matches[0]}{matches[-1]}")
+        #print(f"El codigo es {matches[0]}{matches[-1]}")
         sum += int(matches[0] + matches[-1])
+
+    print(sum)
 
 
 if __name__ == "__main__":
     with open("input_1.txt") as f:
-        input = f.readlines
+        input = f.readlines()
     main(input)
